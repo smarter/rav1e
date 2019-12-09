f5578bda40a8b16bfad039202af973f0d266efb4
Guillaume Martres <smarter@ubuntu.com>
2019-12-09 18:22:11 +0100

    temporal rdo: avoid a division by zero
    When intra_cost = 0, we divide by zero, this is probably OK here because
    of the following max, but it seems wiser to not rely on the behavior of
    division by zero.

--- src/api/internal.rs
+++ src/api/internal.rs
@@ -1010,7 +1010,12 @@ impl<T: Pixel> ContextInner<T> {
                   let future_importance = block_importances[x];
 
                   let propagate_fraction =
-                    (1. - inter_cost / intra_cost).max(0.);
+                    if intra_cost <= inter_cost {
+                      0.
+                    } else {
+                      1. - inter_cost / intra_cost
+                    };
+
                   let propagate_amount = (intra_cost + future_importance)
                     * propagate_fraction
                     / len as f32;
